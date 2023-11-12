e4396706a68b50f01bb2870df0d7915c9df0fee6
Bohdan Kryvetskyy <bkryvetskyi@gmail.com>
2023-11-13 01:37:57 +0200

    Rename and fix implementation class.

---
+++ src/main/java/com/bkryvetskyi/service/data/DataFileReader.java
@@ -0,0 +1,25 @@
+package com.bkryvetskyi.service.data;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bkryvetskyi.model.LapTime;
+import com.bkryvetskyi.model.Racer;
+import com.bkryvetskyi.service.RacerRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * The RaceDataReader class is responsible for reading racer data and lap times from input files.
+ */
+public class DataFileReader {
+    private static final Logger LOGGER = LogManager.getLogger(DataFileReader.class);
+
+
+}
