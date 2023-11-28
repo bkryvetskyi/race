70754d4f3ddcd422afdca337668d0384d37bedf2
Bohdan Kryvetskyy <bkryvetskyi@gmail.com>
2023-11-28 23:19:20 +0200

    Fix name methods.

--- src/main/java/com/bkryvetskyi/Application.java
+++ src/main/java/com/bkryvetskyi/Application.java
@@ -2,19 +2,18 @@ package com.bkryvetskyi;
 
 import com.bkryvetskyi.model.LapTime;
 import com.bkryvetskyi.model.Racer;
-import com.bkryvetskyi.service.BestLapTimeSort;
+import com.bkryvetskyi.service.ReportProcessor;
 import java.util.List;
 
 import com.bkryvetskyi.service.formatting.ResultFormatter;
-import com.bkryvetskyi.service.parser.LapTimeParser;
-import com.bkryvetskyi.service.parser.RacersParser;
+import com.bkryvetskyi.service.parser.Parser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
  * The Application class is the entry point for the racing application.
  */
-public class Application {
+public class  Application {
     private static final Logger LOGGER = LogManager.getLogger(Application.class);
     private static final String START_LOG_FILE_NAME = "start.log";
     private static final String END_LOG_FILE_NAME = "end.log";
@@ -29,9 +28,9 @@ public class Application {
      */
     private static void run() {
         ResultFormatter resultFormatter = new ResultFormatter();
-        BestLapTimeSort sort = new BestLapTimeSort();
+        ReportProcessor reportProcessor = new ReportProcessor();
         List<LapTime> lapTimes = loadRacerData();
-        List<LapTime> bestLapTimes = sort.sortBestLapTimes(lapTimes);
+        List<LapTime> bestLapTimes = reportProcessor.sortBestLapTimes(lapTimes);
         String formattedResults = resultFormatter.formatResults(bestLapTimes);
 
         LOGGER.info(formattedResults);
@@ -42,10 +41,10 @@ public class Application {
      * @return A list of lap times.
      */
     private static List<LapTime> loadRacerData() {
-        RacersParser racersParser = new RacersParser();
-        LapTimeParser lapTimeParser = new LapTimeParser();
+        Parser racersParser = new Parser();
+        ReportProcessor reportProcessor = new ReportProcessor();
         List<Racer> racers = racersParser.parseRacers(RACERS_FILE_NAME);
 
-        return lapTimeParser.parserLapTimes(racers, START_LOG_FILE_NAME, END_LOG_FILE_NAME);
+        return reportProcessor.processLapTimesFromLogs(racers, START_LOG_FILE_NAME, END_LOG_FILE_NAME);
     }
 }
