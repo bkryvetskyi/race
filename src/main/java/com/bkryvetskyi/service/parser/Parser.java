f2a075c9ada5a047205fdcb94d7d44df14a353bb
Bohdan Kryvetskyy <bkryvetskyi@gmail.com>
2023-11-28 23:16:21 +0200

    Added functional with parser dateTime.

--- src/main/java/com/bkryvetskyi/service/parser/Parser.java
+++ src/main/java/com/bkryvetskyi/service/parser/Parser.java
@@ -1,17 +1,26 @@
 package com.bkryvetskyi.service.parser;
 
+import com.bkryvetskyi.Application;
 import com.bkryvetskyi.model.Racer;
 import com.bkryvetskyi.service.data.DataFileReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RacersParser {
+public class Parser {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss.SSS");
+    private static final Logger LOGGER = LogManager.getLogger(Parser.class);
+
     /**
      * Parses the information of racers from a file containing racer data.
      * @param fileName The name of the file containing racer information.
      * @return A list of Racer objects parsed from the file.
      */
+
     public List<Racer> parseRacers(String fileName) {
         List<Racer> racers = new ArrayList<>();
 
@@ -30,5 +39,19 @@ public class RacersParser {
         });
 
         return racers;
+    }
+
+    public LocalDateTime parserDataTime(String dataTimeString) {
+        if (isValidFormat(dataTimeString)) {
+            return LocalDateTime.parse(dataTimeString, FORMATTER);
+        } else {
+            LOGGER.error("The format is not valid, " + dataTimeString);
+            return null;
+        }
+    }
+
+    private boolean isValidFormat(String dataTimeString) {
+        String regex = "[A-Z]{3}\\d{4}-\\d{2}-\\d{2}_\\d{2}:\\d{2}:\\d{2}.\\d{3}";
+        return dataTimeString.matches(regex);
     }
 }
