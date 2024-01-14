5c9075a149cb43fde7af228e811562cf7858bb77
Bohdan Kryvetskyy <bkryvetskyi@gmail.com>
2024-01-14 22:00:59 +0200

    Declared pattern as constant.

--- src/main/java/com/bkryvetskyi/model/LapTime.java
+++ src/main/java/com/bkryvetskyi/model/LapTime.java
@@ -8,6 +8,9 @@ public class LapTime {
     private LocalDateTime dateTime;
     private Duration lapDuration;
 
+    private static final String DURATION_FORMAT_PATTERN = "%02d:%02d:%02d.%03d";
+    private static final String DURATION_FORMAT_PATTERN_WITH_HOURS = "%02d:%02d.%03d";
+
     public LapTime(Racer racer, LocalDateTime dateTime) {
         this.racer = racer;
         this.dateTime = dateTime;
@@ -26,15 +29,15 @@ public class LapTime {
         return dateTime;
     }
 
-    public final String getLapDuration() {
+    public String getLapDuration() {
         long hours = Math.abs(lapDuration.toHours());
         long minutes = Math.abs(lapDuration.toMinutes() % 60);
         long second = Math.abs(lapDuration.getSeconds() % 60);
         long milliseconds = Math.abs(lapDuration.toMillis()) % 1000;
 
         if (hours == 0)
-            return String.format("%02d:%02d.%03d", minutes, second, milliseconds);
+            return String.format(DURATION_FORMAT_PATTERN_WITH_HOURS, minutes, second, milliseconds);
         else
-            return String.format("%02d:%02d:%02d.%03d", hours, minutes, second, milliseconds);
+            return String.format(DURATION_FORMAT_PATTERN, hours, minutes, second, milliseconds);
     }
 }
